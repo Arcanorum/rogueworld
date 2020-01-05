42950589570edbf32458b8acb222343aa9bcded9
Johnathan <johnathan_fisher93@yahoo.co.uk>
2020-01-05 18:10:44 +0000

    Finish adding mage

--- server/src/entities/destroyables/movables/characters/mobs/Mage.js
+++ server/src/entities/destroyables/movables/characters/mobs/Mage.js
@@ -1,17 +1,17 @@
 
-// const Mob = require('./Mob');
+const Mob = require('./Mob');
 
-// class Mage extends Mob {}
-// module.exports = Mage;
+class Mage extends Mob {}
+module.exports = Mage;
 
-// Mage.prototype.registerEntityType();
-// Mage.prototype.assignMobValues("Mage", Mage.prototype);
-// Mage.prototype.CorpseType = require('../../../corpses/CorpseHuman');
-// Mage.prototype.dropList = [
-//     require('./../../../pickups/PickupFabric'),
-//     require('./../../../pickups/PickupFabric'),
-//     require('./../../../pickups/PickupFabric'),
-//     require('./../../../pickups/PickupMageRobe'),
-//     require('./../../../pickups/PickupFireGem'),
-//     require('./../../../pickups/PickupFireStaff'),
-// ];
+Mage.prototype.registerEntityType();
+Mage.prototype.assignMobValues("Mage", Mage.prototype);
+Mage.prototype.CorpseType = require('../../../corpses/CorpseHuman');
+Mage.prototype.dropList = [
+    require('./../../../pickups/PickupFabric'),
+    require('./../../../pickups/PickupFabric'),
+    require('./../../../pickups/PickupFabric'),
+    require('./../../../pickups/PickupMageRobe'),
+    require('./../../../pickups/PickupFireGem'),
+    require('./../../../pickups/PickupFireStaff'),
+];

9ccf2fbc478fdd7e98ec476016ec4b8777af53cf
Johnathan <johnathan_fisher93@yahoo.co.uk>
2020-01-13 01:58:43 +0000

    Handle www to non-www redirect

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,31 +1,15 @@
 
-const serverPortNumber = 4567;
 const fs = require('fs');
 const pathToKeys = '../../../etc/letsencrypt/live/dungeonz.io/';
-const useSecure = fs.existsSync(pathToKeys + 'fullchain.pem');
 
 // Express config.
 const express = require('express');
 const path = require('path');
 const app = express();
 
-if(useSecure){
-    app.all('*', (req, res, next) => {
-        console.log("ensure secure:", 'https://' + req.hostname + req.url);
-        if(req.secure){
-          // OK, continue
-          return next();
-        };
-        // handle port numbers if you need non defaults
-        // res.redirect('https://' + req.host + req.url); // express 3.x
-        res.redirect('https://' + req.hostname + req.url); // express 4.x
-    });
-}
-
 app.use(express.static(path.join(__dirname, '../../client/')));
 
 app.get('/', (req, res) => {
-    res.send("Loading...");
     res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
@@ -46,11 +30,16 @@ if(fs.existsSync(pathToKeys + 'fullchain.pem')){
        key:    fs.readFileSync(pathToKeys + 'privkey.pem'),
     }, app).listen(443);
 
-    console.log("* Started HTTPS and WS servers on port", serverPortNumber);
+    console.log("* Started HTTPS and WS servers on port", 443);
 
+    const redirectApp = express();
+    // Redirect all requests to the HTTP (port 80) server to the HTTPS server.
+    redirectApp.all('*', (req, res, next) => {
+        res.redirect('https://dungeonz.io');
+    });
     // Create a basic HTTP server to catch HTTP 
     // requests and redirect to the HTTPS server.
-    require('http').createServer(app).listen(80);
+    require('http').createServer(redirectApp).listen(80);
 
 }
 // No certificate found, use unsecure connections for development.
@@ -59,10 +48,8 @@ else {
     httpServer = http.createServer(app).listen(80);
 
     console.log("* Could not locate SSL certificate.");
-    console.log("* Started unsecure HTTP and WS servers on port", serverPortNumber);
+    console.log("* Started unsecure HTTP and WS servers on port", 80);
 }
-
-//httpServer.listen(serverPortNumber);
 
 wss = new WebSocket.Server({
     server: httpServer

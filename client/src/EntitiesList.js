88c3c2147e7613297f1bf1d944e5d5ed7c61aad1
Johnathan <johnathan_fisher93@yahoo.co.uk>
2020-10-18 02:24:44 +0100

    Refactor identical pickup classes to be generated from a list instead

--- client/src/EntitiesList.js
+++ client/src/EntitiesList.js
@@ -1,15 +1,46 @@
-// TODO: make some kind of "basic pickups" list type of thing to avoid needing a file each? they are all the same atm...
+import Utils from "./Utils";
+import GenericPickupsList from "./entities/pickups/GenericPickupsList";
 
 /**
- * A list of all client display entities that can be created.
+ * A list of all client display entities that can be instantiated.
+ * Created using all of the JS files found in /entities, to avoid having a huge list of imports.
+ * The list looks like `<FILENAME>: <TYPE/CLASS>`.
+ * @example
+ * {
+ *     Knight: Entity,
+ *     ProjShuriken: Entity,
+ *     PickupFireGem: Pickup, // Specialised pickup class
+ *     PickupIronBar: GenericPickup, // Generated pickup class
+ * }
  * @type {Object}
  */
-export default (ctx => {
-    let keys = ctx.keys();
-    let values = keys.map(ctx);
-    return keys.reduce((object, key, index) => {
-        key = key.split("/").pop().slice(0, -3);
-        object[key] = values[index].default;
+export default ((context) => {
+    let fileNames = context.keys();
+    let values = fileNames.map(context);
+    // Add each class to the list by file name.
+    let entitiesList = fileNames.reduce((object, fileName, index) => {
+        // Trim the ".js" from the end of the file name.
+        fileName = fileName.split("/").pop().slice(0, -3);
+        // Need to use .default to get the class from the file, or would need to actually import it.
+        object[fileName] = values[index].default;
         return object;
     }, {});
+
+    // Add the generic pickups that don't have their own class files.
+    // They get classes made for them on startup.
+    Object.entries(GenericPickupsList).forEach(([key, value]) => {
+        key = "Pickup" + key;
+        // Check for duplicate entries in the list.
+        if(entitiesList[key]) {
+            Utils.warning(
+                "Building entities list. Adding generated pickup class for \"" + key + "\", but type already exists with this key. Skipping. " +
+                "A pickup type should be defined either in a class file (if it does something special), or in the pickups list, but not both."
+            );
+            return;
+        };
+
+        entitiesList[key] = value;
+    });
+
+    return entitiesList;
 })(require.context('./entities/', true, /.js$/));

cdc3485b089ec8cbf86d23f549af62116705ec7c
Johnathan <johnathan_fisher93@yahoo.co.uk>
2020-11-21 18:13:46 +0000

    Fix some mob animations not looping

--- client/src/entities/characters/Hawk.js
+++ client/src/entities/characters/Hawk.js
@@ -1,13 +1,15 @@
 import Character from "./Character";
 
 class Entity extends Character {
-    constructor(x, y, config){
+    constructor(x, y, config) {
         super(x, y, config);
-        
+
         this.displayName.setText(dungeonz.getTextDef("Mob name: Hawk"));
+        this.baseSprite.anims.play(`${this.animationSetName}-${this.direction}`);
     }
 }
 
 Entity.prototype.animationSetName = "hawk";
+Entity.prototype.animationRepeats = true;
 
 export default Entity;
--- client/src/entities/characters/Rat.js
+++ client/src/entities/characters/Rat.js
@@ -1,14 +1,15 @@
 import Character from "./Character";
 
 class Entity extends Character {
-    constructor(x, y, config){
+    constructor(x, y, config) {
         super(x, y, config);
-    
+
         this.displayName.setText(dungeonz.getTextDef("Mob name: Rat"));
         this.baseSprite.setScale(0.5);
     }
 }
 
 Entity.prototype.animationSetName = "rat";
+Entity.prototype.animationRepeats = true;
 
 export default Entity;

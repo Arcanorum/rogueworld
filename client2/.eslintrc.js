b011f5bf33c3b36269c5e7f931a342eb93e218d7
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-02-04 22:32:52 +0800

    UPDATE: reverted eslint max-len back to 100

--- client2/.eslintrc.js
+++ client2/.eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
         "max-len": [
             "error",
             {
-                code: 150,
+                code: 100,
                 ignoreComments: true,
                 ignoreTrailingComments: true,
                 ignoreUrls: true,

c784f84c9c6609eff6021978cb4d2d3bb55f7689
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-03-05 17:07:40 +0000

    Fix partners modal not displaying correctly

--- client/src/components/login/partners/Partners.jsx
+++ client/src/components/login/partners/Partners.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import ioGamesLogo from "../../../assets/images/misc/branding/iogames-space-logo.png";
 import crazyGamesLogo from "../../../assets/images/misc/branding/crazy-games-logo.png";
+import "./Partners.scss";
 
 function Partners({ toggleShowPartners }) {
     return (
-        <div className="partners-cont press-start-font">
-            <div className="partners-close" onClick={toggleShowPartners}>x</div>
-            <div className="partners-header">Thanks to our partners</div>
+        <div className="partners-cont press-start-font" onMouseLeave={toggleShowPartners}>
+            <div className="close" onClick={toggleShowPartners}>x</div>
+            <div className="header">Thanks to our partners</div>
             <a href="http://iogames.space/" rel="noreferrer" target="_blank">
                 <img className="iogames-space-logo" src={ioGamesLogo} />
             </a>

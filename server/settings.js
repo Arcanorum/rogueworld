8260246569945eeace023d1dd802cd67a0940408
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-03-10 01:00:41 +0000

    Lint

--- server/settings.js
+++ server/settings.js
@@ -4,10 +4,10 @@ const yaml = require("js-yaml");
 let settings;
 
 if (fs.existsSync("./settings.yml")) {
-    settings = yaml.safeLoad(fs.readFileSync(__dirname + "/settings.yml", "utf8"));
+    settings = yaml.safeLoad(fs.readFileSync(`${__dirname}/settings.yml`, "utf8"));
 }
 else {
-    settings = yaml.safeLoad(fs.readFileSync(__dirname + "/settings.example.yml", "utf8"));
+    settings = yaml.safeLoad(fs.readFileSync(`${__dirname}/settings.example.yml`, "utf8"));
 }
 
 module.exports = settings;

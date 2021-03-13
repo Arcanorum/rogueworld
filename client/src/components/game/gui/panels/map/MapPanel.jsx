bbaf9bf18567a1a23404308bb2519596f5bf5c8b
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-03-13 16:58:51 +0800

    UPDATED: World map
    Co-authored-by: Johnathan <johnathan_fisher93@yahoo.co.uk>

--- client/src/components/game/gui/panels/map/MapPanel.jsx
+++ client/src/components/game/gui/panels/map/MapPanel.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {
-    MapContainer as Map, TileLayer, Marker, useMapEvents, Tooltip,
+    MapContainer, TileLayer, Marker, useMapEvents, Tooltip,
 } from "react-leaflet";
-import { CRS } from "leaflet";
+import Leaflet, { CRS } from "leaflet";
 import PanelTemplate from "../panel_template/PanelTemplate";
 import mapIcon from "../../../../../assets/images/gui/hud/map-icon.png";
+import playerIcon from "../../../../../assets/images/gui/panels/map/player-marker.png";
+import destinationIcon from "../../../../../assets/images/gui/panels/map/destination-marker.png";
+import "leaflet/dist/leaflet.css";
 import "./MapPanel.scss";
 import { ApplicationState, PlayerState } from "../../../../../shared/state/States";
+
+const markerScale = 4;
+
+const playerMarker = Leaflet.icon({
+    iconUrl: playerIcon,
+    iconSize: [12 * markerScale, 14 * markerScale],
+    iconAnchor: [24, 48],
+});
+
+const locationMarker = Leaflet.icon({
+    iconUrl: destinationIcon,
+    iconSize: [12 * markerScale, 14 * markerScale],
+    iconAnchor: [24, 48],
+});
 
 function LocationMarker() {
     const [position, setPosition] = useState(null);
@@ -34,8 +51,8 @@ function LocationMarker() {
     });
 
     return position === null ? null : (
-        <Marker position={position}>
-            <Tooltip direction="top" offset={[-15, -10]} opacity={1} permanent>
+        <Marker position={position} icon={locationMarker}>
+            <Tooltip className="press-start-font tooltip" direction="top" offset={[0, -44]} opacity={1} permanent>
                 { `${position.lat}, ${position.lng}` }
             </Tooltip>
         </Marker>
@@ -44,7 +61,11 @@ function LocationMarker() {
 
 function MapPanel({ onCloseCallback }) {
     const leafletConfig = {
-        center: [-PlayerState.row + 32, PlayerState.col - 248],
+        center: [
+            // The center is a decimal (with the extra half tile), so round down for display.
+            Math.floor(-PlayerState.row + 32),
+            Math.floor(PlayerState.col - 248 + 0.5),
+        ],
         zoom: 3,
     };
 
@@ -57,24 +78,33 @@ function MapPanel({ onCloseCallback }) {
               icon={mapIcon}
               onCloseCallback={onCloseCallback}
             >
-                <Map
+                <MapContainer
                   center={leafletConfig.center}
                   zoom={leafletConfig.zoom}
                   maxZoom={4}
                   minZoom={1}
                   crs={CRS.Simple}
                   doubleClickZoom={false}
+                  /*
+                   this should restrict the user from panning outside the map and avoid useless request to server,
+                   but can't seem to find a way to dynamically set bounds based on TileLayer's LatLng
+                   arrays are coordinates of southwest and northeast corner of TileLayer
+                   this can cause problem if different map size is loaded
+                  */
+                  // bounds={[[-676, 0], [0, 774]]}
+                  // maxBounds={[[-676, 0], [0, 774]]}
+                  // maxBoundsViscosity={0.9}
                 >
                     <TileLayer url={`${ApplicationState.httpServerURL}/map/{z}/{x}/{y}.png`} />
-                    <Marker position={leafletConfig.center}>
-                        <Tooltip direction="top" offset={[-15, -10]} opacity={1} permanent>
+                    <Marker position={leafletConfig.center} icon={playerMarker}>
+                        <Tooltip className="press-start-font tooltip" direction="top" offset={[0, -44]} opacity={1} permanent>
                             You
                             <br />
                             { `${leafletConfig.center[0]}, ${leafletConfig.center[1]}` }
                         </Tooltip>
                     </Marker>
                     <LocationMarker />
-                </Map>
+                </MapContainer>
             </PanelTemplate>
         </div>
     );

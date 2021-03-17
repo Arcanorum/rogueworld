826bc494da77b78d6922c8be509eb579ffd73e8f
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-03-17 19:49:40 +0000

    Fix dev client build path

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,37 +1,35 @@
-const Utils = require("./Utils");
 const path = require("path");
 const express = require("express");
-
-const app = express();
-const WebSocket = require("ws");
 const http = require("http");
+const WebSocket = require("ws");
+const Utils = require("./Utils");
 const settings = require("../settings");
 
+const app = express();
+
 const httpPort = 4567;
-let httpServer;
-let wss;
 
 // Serve the client locally for development.
 // Production should use a proper file server (i.e. Caddy).
 if (settings.DEV_MODE) {
-    app.use(express.static(path.join(__dirname, "../../client/")));
+    app.use(express.static(path.join(__dirname, "../../client/build")));
 
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../client/index.html"));
+        res.sendFile(path.join(__dirname, "../../client/build/index.html"));
     });
 }
 
-app.get('/map/:z/:x/:y', function (req, res) {
-    const {z, x, y} = req.params;
+app.get("/map/:z/:x/:y", (req, res) => {
+    const { z, x, y } = req.params;
 
     res.sendFile(path.join(__dirname, `../map/leaflet-map/${z}/${x}/${y}`), (err) => {
         if (err) return res.sendStatus(404);
     });
 });
 
-httpServer = http.createServer(app).listen(httpPort);
+const httpServer = http.createServer(app).listen(httpPort);
 
-wss = new WebSocket.Server({
+const wss = new WebSocket.Server({
     server: httpServer,
 });
 

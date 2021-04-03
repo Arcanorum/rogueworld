6d8bfb8fec60afa23b048ba91fd3c992b3649ed5
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-04-03 02:40:57 +0100

    Nerf glory orb

--- server/src/items/GloryOrb.js
+++ server/src/items/GloryOrb.js
@@ -2,7 +2,7 @@ const Item = require("./Item");
 
 class GloryOrb extends Item {
     onUsed() {
-        this.owner.modGlory(1000);
+        this.owner.modGlory(500);
 
         super.onUsed();
     }

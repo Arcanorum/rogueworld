4ba130e1cb63cdc9b43cb213198cb45c6071ea96
ragingpix3l <ragingpix3l@gotoxy.pl>
2021-04-17 21:38:01 +0200

    modified translations import script - importing data from sheets in workbook

--- server/src/scripts/TextDefinitionsParser.js
+++ server/src/scripts/TextDefinitionsParser.js
@@ -6,80 +6,93 @@ const fs = require("fs");
 const Utils = require("../Utils");
 
 const workbook = XLSX.readFile("Dungeonz.io translations.xlsx");
+const firstTabIndex = 2;
+// const sheetName = workbook.SheetNames[0];
+// Utils.message(JSON.stringify(parseSheet(workbook, sheetName)).length);
 
-const firstSheetName = workbook.SheetNames[0];
-// Get the worksheet.
-const worksheet = workbook.Sheets[firstSheetName];
-// Some characters to loop though, to go across the columns.
-const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
-const defsAsJSON = {};
-const idNameCol = "A";
-// The row that the language names are on.
-const languageNameRow = 8;
-// The row that the first definition is on for each column.
-const firstDefRow = 9;
-// The current row being accessed.
-let currentDefRow = 9;
-// The index of the column character in the chars array for the current language. Incremented as the languages are gone through.
-let currentLanguageCharIndex = 1;
-// The value of that index in the chars array.
-let currentLanguageCol = chars[currentLanguageCharIndex];
-// The name of the language i.e. 'English', 'French'.
-let currentLanguageName = worksheet[currentLanguageCol + languageNameRow].v;
-// An object of the text defs that have been added to the current language so far.
-let currentLanguageDefs;
+function parseSheet(sheetName, defs) {
+    // Utils.message(sheetName);
+    // Get the worksheet.
+    const worksheet = workbook.Sheets[sheetName];
+    // Some characters to loop though, to go across the columns.
+    const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
+    const defsAsJSON = defs || {};
+    const idNameCol = "A";
+    // The row that the language names are on.
+    const languageNameRow = 8;
+    // The row that the first definition is on for each column.
+    const firstDefRow = 9;
+    // The current row being accessed.
+    let currentDefRow = 9;
+    // The index of the column character in the chars array for the current language. Incremented as the languages are gone through.
+    let currentLanguageCharIndex = 1;
+    // The value of that index in the chars array.
+    let currentLanguageCol = chars[currentLanguageCharIndex];
+    // The name of the language i.e. 'English', 'French'.
+    let currentLanguageName = worksheet[currentLanguageCol + languageNameRow].v;
+    // An object of the text defs that have been added to the current language so far.
+    let currentLanguageDefs;
 
-// Add an object for the current language.
-defsAsJSON[currentLanguageName] = {};
+    // Add an object for the current language.
+    defsAsJSON[currentLanguageName] = defsAsJSON[currentLanguageName] || {};
 
-while (true) {
-    // Check if this language is valid. The previous one might have been the last one, in which case end.
-    if (currentLanguageName === undefined) break;
+    while (true) {
+        // Check if this language is valid. The previous one might have been the last one, in which case end.
+        if (currentLanguageName === undefined) break;
 
-    // Shorthand.
-    currentLanguageDefs = defsAsJSON[currentLanguageName];
+        // Shorthand.
+        currentLanguageDefs = defsAsJSON[currentLanguageName];
 
-    // Add the ID name as a key. Null by default, value added if found.
-    currentLanguageDefs[worksheet[idNameCol + currentDefRow].v] = null;
+        // Add the ID name as a key. Null by default, value added if found.
+        currentLanguageDefs[worksheet[idNameCol + currentDefRow].v] = null;
 
-    // Check there is a value at this row for the current language.
-    if (worksheet[currentLanguageCol + currentDefRow] !== undefined) {
-        // Add the value.
-        currentLanguageDefs[
-            worksheet[idNameCol + currentDefRow].v
-        ] = worksheet[currentLanguageCol + currentDefRow].v;
+        // Check there is a value at this row for the current language.
+        if (worksheet[currentLanguageCol + currentDefRow] !== undefined) {
+            // Add the value.
+            currentLanguageDefs[
+                worksheet[idNameCol + currentDefRow].v
+            ] = worksheet[currentLanguageCol + currentDefRow].v;
+        }
+
+        // Go down a row to the next definition.
+        currentDefRow += 1;
+
+        // Check there is another ID to add at the next field.
+        if (worksheet[idNameCol + currentDefRow] === undefined) {
+            // At the end of the definitions, so try the next language.
+
+            // Get the language column character along.
+            currentLanguageCharIndex += 1;
+            currentLanguageCol = chars[currentLanguageCharIndex];
+
+            // Check if there is another language. The previous one might have been the last one, in which case end.
+            if (worksheet[currentLanguageCol + languageNameRow] === undefined) break;
+
+            // Get the name of the language.
+            currentLanguageName = worksheet[currentLanguageCol + languageNameRow].v;
+
+            // Add an object for the current language.
+            defsAsJSON[currentLanguageName] = defsAsJSON[currentLanguageName] || {};
+
+            // Go back up to the first definition row so it can start looping down again.
+            currentDefRow = firstDefRow;
+        }
     }
-
-    // Go down a row to the next definition.
-    currentDefRow += 1;
-
-    // Check there is another ID to add at the next field.
-    if (worksheet[idNameCol + currentDefRow] === undefined) {
-        // At the end of the definitions, so try the next language.
-
-        // Get the language column character along.
-        currentLanguageCharIndex += 1;
-        currentLanguageCol = chars[currentLanguageCharIndex];
-
-        // Check if there is another language. The previous one might have been the last one, in which case end.
-        if (worksheet[currentLanguageCol + languageNameRow] === undefined) break;
-
-        // Get the name of the language.
-        currentLanguageName = worksheet[currentLanguageCol + languageNameRow].v;
-
-        // Add an object for the current language.
-        defsAsJSON[currentLanguageName] = {};
-
-        // Go back up to the first definition row so it can start looping down again.
-        currentDefRow = firstDefRow;
-    }
+    return defsAsJSON;
 }
 
+const defsAsJSONmultiPart = {};
+
+for (let i = firstTabIndex; i < workbook.SheetNames.length; i += 1) {
+    parseSheet(workbook.SheetNames[i], defsAsJSONmultiPart);
+    // Utils.message(`${i} page of ${workbook.SheetNames.length}`);
+}
+// Utils.message(JSON.stringify(defsAsJSONmultiPart).length);
 // Write the text defs to the client.
 let dataToWrite = {};
 
 // Turn the data into a string.
-dataToWrite = JSON.stringify(defsAsJSON);
+dataToWrite = JSON.stringify(defsAsJSONmultiPart);
 
 Utils.checkClientCataloguesExists();
 

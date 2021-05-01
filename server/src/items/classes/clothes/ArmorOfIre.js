8b371186ead5fca2e6e5870a7b2a1257d2ce49e0
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-05-01 04:27:58 +0100

    Fix armor of ire infinite loop, reduce damage each time it recurses

--- server/src/items/classes/clothes/ArmorOfIre.js
+++ server/src/items/classes/clothes/ArmorOfIre.js
@@ -9,10 +9,22 @@ class ArmorOfIre extends Clothes {
         // Check the entity can be damaged.
         // Source might be null, when damaged by the environment.
         if (source && source.damage) {
-            // Only damage the source if this player has glory.
-            if (this.owner.glory > Math.abs(damage.amount)) {
-                this.owner.modGlory(-damage.amount);
-                source.damage(damage, this.owner);
+            // Reduce the damage taken each time this method is called.
+            // Most of the time this will only be called once when damaged, but there is a case
+            // where the source of the damage is also using an item like Armor of Ire, so the
+            // damage gets sent back and forth between this method on both items.
+            // This reduction each time means there will eventually be an ending point at 0 damage dealt.
+            const effectiveDamage = Math.floor((damage - (damage * 0.5)) - 1);
+
+            if (effectiveDamage > 0) {
+                // Only damage the source if this player has glory.
+                if (this.owner.glory > Math.abs(effectiveDamage)) {
+                    this.owner.modGlory(-effectiveDamage);
+                    source.damage(
+                        effectiveDamage,
+                        this.owner,
+                    );
+                }
             }
         }
 

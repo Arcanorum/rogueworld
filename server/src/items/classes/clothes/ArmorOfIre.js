d88e8ec6dacf29c878180931145da32b83991ebd
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-05-01 17:23:38 +0100

    Fix armor of ire wrong damage amount passed

--- server/src/items/classes/clothes/ArmorOfIre.js
+++ server/src/items/classes/clothes/ArmorOfIre.js
@@ -1,27 +1,29 @@
+const Damage = require("../../../gameplay/Damage");
 const Clothes = require("./Clothes");
 
 class ArmorOfIre extends Clothes {
-    modDurability() {
-        // Overwrite this with nothing so it doesn't lose durability.
-    }
-
     onDamaged(damage, source) {
         // Check the entity can be damaged.
         // Source might be null, when damaged by the environment.
         if (source && source.damage) {
-            // Reduce the damage taken each time this method is called.
+            // Reduce the damage dealt each time this method is called.
             // Most of the time this will only be called once when damaged, but there is a case
             // where the source of the damage is also using an item like Armor of Ire, so the
             // damage gets sent back and forth between this method on both items.
             // This reduction each time means there will eventually be an ending point at 0 damage dealt.
-            const effectiveDamage = Math.floor((damage - (damage * 0.5)) - 1);
+            const effectiveDamageAmount = Math.floor(damage.amount - 1);
 
-            if (effectiveDamage > 0) {
+            if (effectiveDamageAmount > 0) {
                 // Only damage the source if this player has glory.
-                if (this.owner.glory > Math.abs(effectiveDamage)) {
-                    this.owner.modGlory(-effectiveDamage);
+                if (this.owner.glory > Math.abs(effectiveDamageAmount)) {
+                    this.owner.modGlory(-effectiveDamageAmount);
+
                     source.damage(
-                        effectiveDamage,
+                        new Damage({
+                            amount: effectiveDamageAmount,
+                            types: damage.types,
+                            armourPiercing: damage.armourPiercing,
+                        }),
                         this.owner,
                     );
                 }

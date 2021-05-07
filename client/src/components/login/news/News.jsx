3f0e6a014e2708bd80c70719296f8f0106c5e4bd
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-05-07 05:29:39 +0100

    Update 9 changelog

--- client/src/components/login/news/News.jsx
+++ client/src/components/login/news/News.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import image from "../../../assets/images/misc/news/news-01-04-2021.png";
+import image from "../../../assets/images/misc/news/news-07-05-2021.png";
 import "./News.scss";
 
 function News() {
     return (
         <div className="news-cont" onClick={() => { window.open("/changelog.txt", "_blank"); }}>
             <h1 className="news-header">News</h1>
-            <p className="news-date">01/04/2021</p>
+            <p className="news-date">07/05/2021</p>
             <p className="news-title">
-                Update 8
+                Update 9
                 <br />
-                Items rework
+                City redesign
             </p>
             <img className="news-image" draggable="false" src={image} />
         </div>

5ff407da34149291e74e772fc05dc865ad2d2738
Johnathan <johnathan_fisher93@yahoo.co.uk>
2021-09-18 03:28:48 +0100

    Add tailwind self damage 0 -> 6

--- server/src/entities/classes/destroyables/movables/projectiles/ProjTailwind.js
+++ server/src/entities/classes/destroyables/movables/projectiles/ProjTailwind.js
@@ -1,3 +1,4 @@
+const Damage = require("../../../../../gameplay/Damage");
 const Projectile = require("./Projectile");
 
 class ProjTailwind extends Projectile {
@@ -6,6 +7,15 @@ class ProjTailwind extends Projectile {
         if (collidee instanceof ProjTailwind) return;
 
         this.pushBackCollidee(collidee, 2);
+
+        collidee.damage(
+            new Damage({
+                amount: this.damageAmount,
+                types: this.damageTypes,
+                armourPiercing: this.damageArmourPiercing,
+            }),
+            this.source,
+        );
     }
 }
 module.exports = ProjTailwind;

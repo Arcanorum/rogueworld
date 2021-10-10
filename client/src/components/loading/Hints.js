8d52d9f80616ea0ab31263739749e40ea17802e5
Brendan C <30474072+brendan-c@users.noreply.github.com>
2021-10-10 18:45:27 -0400

    Add shared glory hint (#210)
    * Fixed chat box scrolling issue #155
    
    * added shift + click drop functionality to inventory panel
    
    * test
    
    * Delete Untitled.png
    
    * Add shared glory hint

--- client/src/components/loading/Hints.js
+++ client/src/components/loading/Hints.js
@@ -9,6 +9,7 @@ import discord from "../../assets/images/misc/hints/discord.png";
 import dungeonDoor from "../../assets/images/misc/hints/dungeon-door.png";
 import bluecaps from "../../assets/images/misc/hints/bluecaps.png";
 import redcaps from "../../assets/images/misc/hints/redcaps.png";
+import sharedGlory from "../../assets/images/misc/hints/shared-glory.png";
 
 class Hint {
     constructor(image, textDefId) {
@@ -29,6 +30,7 @@ const Hints = [
     new Hint(dungeonDoor, "Dungeon doors"),
     new Hint(bluecaps, "Bluecaps location"),
     new Hint(redcaps, "Redcaps location"),
+    new Hint(sharedGlory, "Shared glory"),
 ];
 
 export default Hints;

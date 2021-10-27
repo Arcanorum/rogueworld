33437a30582d1778024be9f68700015b7a4cd570
Brendan C <30474072+brendan-c@users.noreply.github.com>
2021-10-26 20:00:45 -0400

    Make priests heal nearby players who aren't in combat (#213)
    * Fixed chat box scrolling issue #155
    
    * added shift + click drop functionality to inventory panel
    
    * test
    
    * Delete Untitled.png
    
    * Update .loading-hint-image CSS
    
    Loading hint image should no longer text around
    The image retains it's previous (more or less), and keeps it's aspect ratio
    
    * Priests heal nearby players out of combat
    
    * Revert unrelated change
    
    * Clear interval on destroy, Increase heal amount
    
    * fix comment

--- server/src/entities/classes/destroyables/movables/characters/mobs/merchants/PriestMerchant.js
+++ server/src/entities/classes/destroyables/movables/characters/mobs/merchants/PriestMerchant.js
@@ -1,7 +1,36 @@
 const Merchant = require("./Merchant");
 const ShopTypesList = require("../../../../../../../shops/ShopTypesList");
+const Heal = require("../../../../../../../gameplay/Heal");
 
-class PriestMerchant extends Merchant {}
+class PriestMerchant extends Merchant {
+    constructor(config) {
+        super(config);
+        // Heal nearby players every 2 seconds
+        this.healInterval = setInterval(this.healNearbyPlayers.bind(this), 2000);
+    }
+
+    healNearbyPlayers() {
+        // Scan tiles within 2 tile radius of Priest
+        this.board.getTilesInEntityRange(this, 2).forEach((boardTile) => {
+            // Only heal Players (not destroyables)
+            Object.values(boardTile.players).forEach((player) => {
+                // If player is in combat, do not heal
+                if (player.isInCombat()) return;
+
+                // Don't heal if player is already at max hitpoints
+                if (player.hitPoints < player.maxHitPoints) {
+                    // Heal 5 hitpoints
+                    player.heal(new Heal(20));
+                }
+            });
+        });
+    }
+
+    onDestroy() {
+        clearInterval(this.healInterval);
+        super.onDestroy();
+    }
+}
 
 PriestMerchant.prototype.shop = ShopTypesList.Priest;
 

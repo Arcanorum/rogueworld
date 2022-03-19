601a19495593b8e9d0ccbfed95161feae9f5b9b5
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-03-19 18:16:15 +0000

    Add more debug logs

--- git-webhook.js
+++ git-webhook.js
@@ -4,9 +4,15 @@ const crypto = require('crypto');
 const port = 8080;
 const secret = process.argv[2];
 
+/**
+ * TODO: doc this
+ */
+
 http.createServer(function(req, res) {
+    console.log('Request received');
+
     req.on('data', function(chunk) {
-        console.log('Data received');
+        console.log('Data event');
 
         if(secret) {
             console.log('Secret provided, checking signature');
@@ -18,6 +24,7 @@ http.createServer(function(req, res) {
         console.log('Pulling from github');
         exec('git pull');
         console.log('Pulled from github');
+
         // exec('npm run ');
     });
 

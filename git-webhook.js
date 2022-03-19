af0e1cced6722c399f5143082d942746e7600ef2
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-03-19 03:54:10 +0000

    Add more bebug logs

--- git-webhook.js
+++ git-webhook.js
@@ -2,17 +2,23 @@ const http = require('http');
 const crypto = require('crypto');
 
 const port = 8080;
+const secret = process.argv[2];
 
 http.createServer(function(req, res) {
     req.on('data', function(chunk) {
         console.log('Data received');
-        let sig = `sha1=${crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex')}`;
 
-        if (req.headers['x-hub-signature'] === sig) {
-            exec('git pull');
-            console.log('Pulled from github');
-            // exec('npm run ');
+        if(secret) {
+            console.log('Secret provided, checking signature');
+            let sig = `sha1=${crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex')}`;
+
+            if (req.headers['x-hub-signature'] !== sig) return;
         }
+
+        console.log('Pulling from github');
+        exec('git pull');
+        console.log('Pulled from github');
+        // exec('npm run ');
     });
 
     res.end();

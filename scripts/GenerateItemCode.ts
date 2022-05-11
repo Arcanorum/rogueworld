c38f7d9fc4eeb10b351c8db251212c3e59c26751
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-05-12 00:47:02 +0100

    Add generate item code script

---
+++ scripts/GenerateItemCode.ts
@@ -0,0 +1,20 @@
+import { getRandomElement, message } from '@dungeonz/utils';
+
+(() => {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let code = '';
+    code += getRandomElement(letters);
+    code += getRandomElement(letters);
+    code += getRandomElement(letters);
+    code += getRandomElement(letters);
+    code += getRandomElement(numbers);
+    code += getRandomElement(numbers);
+    code += getRandomElement(numbers);
+    code += getRandomElement(numbers);
+
+    message('Generated item code:');
+    // eslint-disable-next-line no-console
+    console.log(code);
+})();

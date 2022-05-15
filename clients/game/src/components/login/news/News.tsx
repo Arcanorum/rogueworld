c25ea87c75f87f249ff13d46840e99410e341273
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-05-15 17:42:45 +0100

    update news

--- clients/game/src/components/login/news/News.tsx
+++ clients/game/src/components/login/news/News.tsx
@@ -1,15 +1,16 @@
-import image from '../../../assets/images/misc/news/news-05-09-2021.png';
+import image from '../../../assets/images/misc/news/news-15-05-2022.png';
 import styles from './News.module.scss';
 
 function News() {
     return (
         <div className={styles['news-cont']} onClick={() => { window.open('/changelog.txt', '_blank'); }}>
             <h1 className={styles['news-header']}>News</h1>
-            <p className={styles['news-date']}>05/09/2021</p>
+            <p className={styles['news-date']}>15/05/2022</p>
             <p className={styles['news-title']}>
-                Update 12
+                Release
                 <br />
-                Craftable relics
+                <br />
+                Welcome to Rogueworld
             </p>
             <img className={styles['news-image']} draggable="false" src={image.src} />
         </div>

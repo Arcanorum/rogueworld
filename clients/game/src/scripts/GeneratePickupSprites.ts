82c71a99ec8a28213359cc0cc2ee71b557f10385
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-05-17 00:02:20 +0100

    Add item pickup sprite generation script, remove existing pickup assets

--- clients/game/src/scripts/GeneratePickupSprites.ts
+++ clients/game/src/scripts/GeneratePickupSprites.ts
@@ -1,9 +1,9 @@
 // Generates the pickup sprite image files from the item icon sources.
 // Saves having to manually draw new images for every item type.
 
-import outline from 'outline-image';
-import path from 'path';
-import fs from 'fs';
+const outline = require('outline-image');
+const path = require('path');
+const fs = require('fs');
 
 const itemsPath = path.join(__dirname, '../assets/images/gui/items/');
 const pickupsPath = path.join(__dirname, '../assets/images/entities/pickups/');

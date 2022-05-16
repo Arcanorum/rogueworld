46b2bea3d75f66d3b2a47e39d7c31118df68af55
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-05-16 23:14:07 +0100

    Remove unused event type

--- clients/game/src/shared/UseItem.ts
+++ clients/game/src/shared/UseItem.ts
@@ -1,9 +1,6 @@
-import PubSub from 'pubsub-js';
 import { ApplicationState } from './state';
-import { USED_ITEM } from './EventTypes';
 import ItemState from './ItemState';
 
 export default (itemState: ItemState) => {
-    PubSub.publish(USED_ITEM, itemState);
     ApplicationState.connection?.sendEvent('use_item', itemState.slotIndex);
 };

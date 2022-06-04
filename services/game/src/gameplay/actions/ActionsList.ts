27205c9c477c7d49d0ddcdd411285a03fb4da9b2
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-06-04 20:13:35 +0100

    Add spit venom action - Add new action icons

--- services/game/src/gameplay/actions/ActionsList.ts
+++ services/game/src/gameplay/actions/ActionsList.ts
@@ -10,6 +10,8 @@ function addAction(name: string, duration: number, run: ActionFunction, config: 
 
 addAction('punch', 1000, basicAttack, { amount: 10, types: [DamageTypes.Physical], penetration: 0 });
 addAction('bite', 2000, basicAttack, { amount: 7, types: [DamageTypes.Physical], penetration: 50 });
+addAction('spit-venom', 2000, basicAttack, { amount: 7, types: [DamageTypes.Biological], penetration: 50 });
+addAction('iron-sword', 2000, basicAttack, { amount: 7, types: [DamageTypes.Physical], penetration: 50 });
 addAction('fireball', 2000, basicAttack, { amount: 10, types: [DamageTypes.Magical], penetration: 50 });
 
 export default ActionsList;

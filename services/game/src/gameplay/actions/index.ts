e55450e4659dae0ce23743e7a848d23fd682d780
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-06-07 19:33:10 +0100

    Add iron sword action

--- services/game/src/gameplay/actions/index.ts
+++ services/game/src/gameplay/actions/index.ts
@@ -9,6 +9,7 @@ import './items';
 
 addAction('punch', 1000, basicAttack, { amount: 10, types: [DamageTypes.Physical], penetration: 0 });
 addAction('bite', 2000, basicAttack, { amount: 7, types: [DamageTypes.Physical], penetration: 50 });
+addAction('iron-sword', 2000, basicAttack, { amount: 7, types: [DamageTypes.Physical], penetration: 50 });
 addAction('spit-venom', 2000, basicAttack, { amount: 7, types: [DamageTypes.Biological], penetration: 50 });
 addAction('fireball', 2000, applyStatusEffect, { StatusEffect: Burn });
 

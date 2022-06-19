c3430de68415e24c5ef88a1aa7c9c4ee7feacea3
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-06-19 17:22:29 +0100

    Ignore test generated file

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
     preset: '@shelf/jest-mongodb',
     transform: tsjPreset.transform,
+    watchPathIgnorePatterns: ['globalConfig'],
 };

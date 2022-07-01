d0938b09e54c5be259a31577984cc7c4e70d2029
Johnathan <johnathan_fisher93@yahoo.co.uk>
2022-07-02 00:03:38 +0100

    Update linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,171 +1,64 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
     plugins: [
-        'import',
-    ],
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:import/typescript',
-        'plugin:@typescript-eslint/recommended',
+        '@typescript-eslint',
     ],
     rules: {
         indent: [
             'error',
             4,
         ],
-        'max-len': [
-            'error',
-            {
-                code: 100,
-                ignoreComments: true,
-                ignoreTrailingComments: true,
-                ignoreUrls: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-                ignoreRegExpLiterals: true,
-            },
-        ],
-        'no-unused-vars': [
-            'warn',
-        ],
-        'no-param-reassign': [
-            'off',
-        ],
-        'no-new': [
-            'off',
-        ],
-        'import/no-dynamic-require': [
-            'off',
-        ],
-        'class-methods-use-this': [
-            'off',
-        ],
-        'max-classes-per-file': [
-            'off',
-        ],
-        quotes: [
-            'error',
-            'single',
-        ],
-        'brace-style': [
-            'warn',
-            'stroustrup',
-            { allowSingleLine: true },
-        ],
-        'linebreak-style': [
-            'off',
-        ],
-        'no-underscore-dangle': [
-            'off',
-        ],
-        'import/no-unresolved': [
-            'error',
-        ],
-        'import/no-extraneous-dependencies': [
-            'off',
-        ],
-        'react/react-in-jsx-scope': [
-            'off',
-        ],
-        semi: [
-            'error',
-            'always',
-        ],
-        'comma-dangle': [
-            'error',
-            'always-multiline',
-        ],
-        'eol-last': [
-            'error',
-            'always',
-        ],
-        'block-spacing': [
-            'error',
-            'always',
-        ],
-        'no-multi-spaces': [
-            'error',
-        ],
-        'no-trailing-spaces': [
-            'error',
-        ],
-        'space-infix-ops': [
-            'error',
-            { 'int32Hint': false },
-        ],
-        'template-curly-spacing': [
-            'error',
-            'never',
-        ],
-        'object-curly-spacing': [
-            'error',
-            'always',
-        ],
-        'array-bracket-spacing': [
-            'error',
-            'never',
-        ],
-        'padded-blocks': [
-            'error',
-            'never',
-        ],
-        'prefer-template': [
-            'error',
-        ],
-        'comma-spacing': [
-            'error',
-        ],
-        'object-shorthand': [
-            'error',
-        ],
-        'space-before-blocks': [
-            'error',
-        ],
-        'space-before-function-paren': [
-            'error',
-            'never',
-        ],
-        'space-in-parens': [
-            'error',
-        ],
-        'key-spacing': [
-            'error',
-        ],
-        'arrow-spacing': [
-            'error',
-        ],
-        'no-multiple-empty-lines': [
-            'error',
-        ],
-        '@typescript-eslint/no-explicit-any': [
-            'off',
-        ],
-        '@typescript-eslint/semi': [
-            'error',
-        ],
-        '@typescript-eslint/no-var-requires': [
-            'off',
-        ],
-        '@typescript-eslint/member-delimiter-style': [
-            'warn',
-            {
-                'multiline': {
-                    'delimiter': 'semi',
-                    'requireLast': true,
-                },
-                'singleline': {
-                    'delimiter': 'semi',
-                    'requireLast': false,
-                },
-            },
-        ],
+        // 'space-infix-ops': [
+        //     'error',
+        //     { 'int32Hint': false },
+        // ],
     },
+
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            extends: [
+                'airbnb-base',
+                'airbnb-typescript/base',
+            ],
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+            rules: {
+                "@typescript-eslint/indent": [
+                    "error",
+                    4
+                ],
+                '@typescript-eslint/brace-style': [
+                    'warn',
+                    'stroustrup',
+                    { allowSingleLine: true },
+                ],
+                'no-new': [
+                    'off',
+                ],
+                'import/no-cycle': [
+                    'off',
+                ],
+                'import/no-extraneous-dependencies': [
+                    'off',
+                ],
+                'no-underscore-dangle': [
+                    'off',
+                ],
+                'no-unused-vars': [
+                    'warn',
+                ],
+                'class-methods-use-this': [
+                    'warn',
+                ],
+                '@typescript-eslint/no-unused-vars': [
+                    'warn',
+                ],
+            }
+        }
+    ],
     env: {
         es6: true,
         node: true,
